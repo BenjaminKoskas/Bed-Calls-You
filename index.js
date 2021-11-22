0b3a62d261838dbc3d1d6df666b9d404ebaaae71
BenjaminKoskas <benjaminkoskas9@gmail.com>
2021-11-22 20:16:18 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const discord = require("discord.js");
+const client = new discord.Client({
+    intents: [
+        discord.Intents.FLAGS.GUILDS,
+        discord.Intents.FLAGS.GUILD_MESSAGES,
+        discord.Intents.FLAGS.DIRECT_MESSAGES,
+        discord.Intents.FLAGS.GUILD_MEMBERS,
+        discord.Intents.FLAGS.GUILD_PRESENCES
+    ]
+});
+const guild = client.guilds.cache.get("688756207661613095");
+const channel = guild.channels.cache.get("688788915964084332");
+const user = guild.members.cache.get("658007339860885525");
+let date;
+let hour;
+let min;
+let sec;
+
+client.login("OTEyMDYzMzc3OTE0ODU1NTE0.YZqfNg.pfSOY5A2vh_ZynQtvU0HyyW7Gg8");
+
+client.once("ready", () => {
+    console.log("Bot initialized");  
+    let interval = setInterval(() => {
+        date = new Date();
+        hour = date.getHours();
+        min = date.getMinutes();
+        sec = date.getSeconds();
+        if (hour === 22 && min === 50 && sec === 0)     
+            channel.send(`${user.toString()} Dans 10 min au lit !`);
+        else if (hour === 22 && min === 55 && sec === 0)     
+            channel.send(`${user.toString()} Dans 5 min au lit !`);
+        else if (hour === 23 && min === 0 && sec === 0)     
+            channel.send(`${user.toString()} Allez hop au lit !`);
+    }, 1000);
+});
